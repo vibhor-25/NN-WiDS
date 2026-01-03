aed8b824b5e50b5b332787dc0ebbb29606ef8fc4
SubramanyaAshok <subramanyapennayakar@gmail.com>
2026-01-03 16:20:39 +0530

    Removed old Assignment 2 files

--- subramanya-ashok/assignment2/fruitproduction.py
+++ subramanya-ashok/assignment2/fruitproduction.py
@@ -1,11 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder
 
 # 1."fruit production"
 
